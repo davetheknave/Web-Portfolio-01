14a0fb5741be9e0f11d2dfbc20610dce0aae0374
davetheknave <knavedavid@gmail.com>
2019-07-18 17:17:31 -0400

    Fix infinite spiro bug

--- src/animations/spiro/spiro.js
+++ src/animations/spiro/spiro.js
@@ -6,35 +6,60 @@ class SpiroBackground extends React.Component {
     constructor(props){
         super(props);
         this.createRandomSpiro = this.createRandomSpiro.bind(this);
+        this.createManySpiros = this.createManySpiros.bind(this);
         this.state = {
             spiros: [],
+            active: [],
             interval: 700,
+            maxSpiros: 7
         }
+        this.currentIndex = 0;
     }
     componentDidMount(){
+        this.createManySpiros(3);
         var intervalID = setInterval(this.createRandomSpiro, this.state.interval);
         this.setState({intervalID: intervalID});
-        this.createManySpiros(3);
     }
     componentWillUnmount(){
         clearInterval(this.state.intervalID);
     }
     createManySpiros(number){
+        var s = [];
         for(var i = 0; i < number; i++){
             this.createRandomSpiro();
         }
     }
     createRandomSpiro(){
-        let duration = this.random(1500, 2000);
-        let size = this.random(50, 400);
-        let rotation = this.random(20, 50);
-        let distance = this.random(50, 200);
-        let opacity = 71/size;
-        let x = this.random(-20, 100);
-        let y = this.random(0, 100);
-        let spiro = (<Spiro x={x} y={y} duration={duration} size={size} rotation={rotation} distance={distance} opacity={opacity}/>);
-        this.state.spiros.push(spiro);
-        this.setState({spiros: this.state.spiros});
+        if(this.state.active[this.currentIndex]){
+            return;
+        }
+        const duration = this.random(1500, 4000);
+        const size = this.random(50, 300);
+        const rotation = -this.random(20, 50);
+        const distance = -this.random(50, 200);
+        const opacity = 71/size;
+        const x = this.random(-10, 90);
+        const y = this.random(10, 90);
+
+        const index = this.currentIndex;
+        const spiro = (<Spiro x={x} y={y} duration={duration} size={size} rotation={rotation} distance={distance} opacity={opacity} complete={() => {
+            const a = this.state.active;
+            a[index] = false;
+            this.setState({active: a});
+        }}/>);
+        
+        const a = this.state.spiros;
+        a[this.currentIndex] = spiro;
+        
+        const b = this.state.active;
+        b[this.currentIndex] = true;
+
+        this.setState({spiros: a, active: b});
+
+        this.currentIndex += 1;
+        if (this.currentIndex >= this.state.maxSpiros){
+            this.currentIndex = 0;
+        }
     }
     random(lowest, highest){
         // Returns an integer between the lowest and highest value, inclusive
@@ -48,34 +73,34 @@ class SpiroBackground extends React.Component {
 class Spiro extends React.Component {
     constructor(props){
         super(props);
-        this.state = {
-            duration: props.duration,
-            size: props.size,
-            rotation: -props.rotation,
-            distance: props.distance,
-            opacity: props.opacity,
-        }
+
         this.ref = React.createRef();
         this.ref2 = React.createRef();
     }
+    componentDidUpdate(){
+        this.timeline.restart();
+    }
     componentDidMount(){
+        this.animate();
+    }
+    animate(){
         const opacityDuration = 1500;
-        const opacity = .5;
 
-        const timeline = anime.timeline({
+        this.timeline = anime.timeline({
             targets: [this.ref.current, this.ref2.current],
             easing: 'linear',
+            complete: this.props.complete,
         });
-        timeline.add({
-            opacity: this.state.opacity,
+        this.timeline.add({
+            opacity: this.props.opacity,
             duration: opacityDuration,
         }, 0);
-        timeline.add({  // Rotatoin
-            translateY: -this.state.distance,
-            rotate: this.state.rotation,  // Rotate must be after translate, or it'll move in a loop
-            duration: this.state.duration + (opacityDuration*2),
+        this.timeline.add({  // Rotatoin
+            translateY: this.props.distance,
+            rotate: this.props.rotation,  // Rotate must be after translate, or it'll move in a loop
+            duration: this.props.duration + (opacityDuration*2),
         }, 0);
-        timeline.add({
+        this.timeline.add({
             opacity: 0,
             duration: opacityDuration,
         }, "-="+opacityDuration);
@@ -83,8 +108,8 @@ class Spiro extends React.Component {
     render() {
         return (
             <div>
-                <Graphic style={{left: this.props.x+"vw", top: this.props.y+"vh"}} width={this.state.size} height={this.state.size} className="spiro spiroBG" ref={this.ref}/>
-                <Graphic style={{left: this.props.x+"vw", top: this.props.y+"vh"}} width={this.state.size} height={this.state.size} className="spiro" ref={this.ref2}/>
+                <Graphic style={{left: this.props.x+"vw", top: this.props.y+"vh"}} width={this.props.size} height={this.props.size} className="spiro spiroBG" ref={this.ref}/>
+                <Graphic style={{left: this.props.x+"vw", top: this.props.y+"vh"}} width={this.props.size} height={this.props.size} className="spiro" ref={this.ref2}/>
             </div>
         )
     }
